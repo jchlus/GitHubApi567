24e3fab7d51b5254588afe6a2e5d5d196684adb0
jchlus <33561386+jchlus@users.noreply.github.com>
2019-09-27 15:37:45 -0400

    take 2
    push 2

--- GithubAPI567.py
+++ GithubAPI567.py
@@ -8,3 +8,28 @@ Created on Fri Sep 27 12:46:14 2019
 import requests
 import json
 
+username = input("What is your github user name: ")
+
+def getUser(variable):
+    
+    url = requests.get("https://api.github.com/users/" + variable + "/repos")
+    x = json.loads(url.text)
+
+    i = 0
+    repos = []
+    while i < len(x):
+        repos.append(x[i]['name'])
+        i = i + 1
+    
+    for repository in repos:
+        commits = requests.get("https://api.github.com/repos/" + variable + "/" + repository + "/commits")
+        y = json.loads(commits.text)
+        
+        t = 0
+        numberCommit = []
+        while t < len(y):
+            numberCommit.append(y[t]['"commit"'])
+            t = t + 1
+            
+        print('Repo: ' + repository + 'Number of commits: ' + len(numberCommit))       
+getUser(username)
